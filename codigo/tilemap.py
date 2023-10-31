0bab599c450caf957cadc2c60bf36fa08c8bc100
luabatepietro <luabatepietroapple@icloud.com>
2023-10-31 08:57:45 -0300

    mapas

--- codigo/tilemap.py
+++ codigo/tilemap.py
@@ -3,17 +3,8 @@ PHYSICS_TILES = {'grass', 'stone'}
 AUTOTILE_TYPES = {'grass', 'stone'}
 import pygame
 import json
-AUTOTILE_MAP = {
-    tuple(sorted([(1, 0), (0, 1)])): 0,
-    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
-    tuple(sorted([(-1, 0), (0, 1)])): 2,
-    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
-    tuple(sorted([(-1, 0), (0, -1)])): 4,
-    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 5,
-    tuple(sorted([(1, 0), (0, -1)])): 6,
-    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
-    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8,
-}
+
+
 class Tilemap:
     def __init__(self, jogo, tile_size=16):
         self.jogo = jogo
@@ -74,19 +65,6 @@ class Tilemap:
                 rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
     
-    def autotile(self):
-        for loc in self.tilemap:
-            tile = self.tilemap[loc]
-            neighbors = set()
-            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
-                check_loc = str(tile['pos'][0] + shift[0]) + ';' + str(tile['pos'][1] + shift[1])
-                if check_loc in self.tilemap:
-                    if self.tilemap[check_loc]['type'] == tile['type']:
-                        neighbors.add(shift)
-            neighbors = tuple(sorted(neighbors))
-            if (tile['type'] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
-                tile['variant'] = AUTOTILE_MAP[neighbors]
-
     def remove_tile(self, pos):
         tile_loc = str(pos[0]) + ';' + str(pos[1])
         if tile_loc in self.tilemap:
